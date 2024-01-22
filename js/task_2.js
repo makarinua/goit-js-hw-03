cb8e398e266c1fc10845af6344798d7a7374a3b0
makarinua <makarinua@gmail.com>
2024-01-22 15:59:09 +0200

    22012024

---
+++ js/task_2.js
@@ -0,0 +1,16 @@
+function makeArray(firstArray, secondArray, maxLength) {
+    let ollArray = firstArray.concat(secondArray);
+    let lengthArray = ollArray.length;
+    if(lengthArray>maxLength){
+        return ollArray.slice(0, maxLength);
+    }else{
+        return ollArray;
+    }
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
